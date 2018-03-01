c2653a0158db906be532b0cd4016f36053f31360
as.kuznecov <as.kuznecov@tensor.ru>
2018-03-01 15:23:56 +0300

    remove eb_header

--- src/ElasticBuilder.js
+++ src/ElasticBuilder.js
@@ -1,60 +1,57 @@
 Vue.component('ElasticBuilder', {
   template: `
     <div class="ElasticBuilder">
-      <div class="ElasticBuilder__head">
-        
-        <div class="ElasticBuilder__row0">
-          Builder
-        </div>
-        
-        <div class="ElasticBuilder__row1">
-          <label class="ElasticBuilder__dataset-name">
-            <div class="ElasticBuilder__label-text">Dataset name</div>
-            <input class="ElasticBuilder__label-option" type="text" v-model="config.DatasetName">
-          </label>
+      <div class="ElasticBuilder__row0">
+        Builder
+      </div>
       
-          <label class="ElasticBuilder__card-type">
-            <div class="ElasticBuilder__label-text">Card type</div>
-            <select class="ElasticBuilder__label-option" v-model="config.CardType">
-              <option v-for="type of availableCardTypes" :value="type">
-                {{type}}
-              </option>
-            </select>
-          </label>
-      
-          <label class="ElasticBuilder__filters-relation">
-            <div class="ElasticBuilder__label-text">Filters Relation</div>
-            <input class="ElasticBuilder__label-option" type="text" pattern="(\\d{1,2}/)+\\d" value="1/1/1" @blur="onFiltersRelation" @keyup.enter="onFiltersRelation">
-          </label>
-      
-          <label class="ElasticBuilder__json">
-            <div class="ElasticBuilder__label-text">JSON</div>
-            <div class="ElasticBuilder__label-option">{{jsonFileName}}</div>
-            <input type="file" accept=".json" @change="readFile">
-          </label>
-          
-          <label class="ElasticBuilder__style">
-            <div class="ElasticBuilder__label-text">Style</div>
-            <div class="ElasticBuilder__label-option">{{styleFileName}}</div>
-            <input type="file" accept=".css" @change="readCss">
-          </label>
-        </div>
+      <div class="ElasticBuilder__row1">
+        <label class="ElasticBuilder__dataset-name">
+          <div class="ElasticBuilder__label-text">Dataset name</div>
+          <input class="ElasticBuilder__label-option" type="text" v-model="config.DatasetName">
+        </label>
+    
+        <label class="ElasticBuilder__card-type">
+          <div class="ElasticBuilder__label-text">Card type</div>
+          <select class="ElasticBuilder__label-option" v-model="config.CardType">
+            <option v-for="type of availableCardTypes" :value="type">
+              {{type}}
+            </option>
+          </select>
+        </label>
+    
+        <label class="ElasticBuilder__filters-relation">
+          <div class="ElasticBuilder__label-text">Filters Relation</div>
+          <input class="ElasticBuilder__label-option" type="text" pattern="(\\d{1,2}/)+\\d" value="1/1/1" @blur="onFiltersRelation" @keyup.enter="onFiltersRelation">
+        </label>
+    
+        <label class="ElasticBuilder__json">
+          <div class="ElasticBuilder__label-text">JSON</div>
+          <div class="ElasticBuilder__label-option">{{jsonFileName}}</div>
+          <input type="file" accept=".json" @change="readFile">
+        </label>
         
-        <div class="ElasticBuilder__filters" ref="filters">
-          <label v-if="fields.length" v-for="(relation, idx) of config.FiltersRelation">
-            <div class="ElasticBuilder__label-text">F{{idx}}</div>
-            <select class="ElasticBuilder__label-option" @change="onSelectFilter">
-              <option v-for="(field, fieldIdx) of fields" :value="field" :selected="fieldIdx === idx">
-                {{field}}
-              </option>
-            </select>
-          </label>
-        </div>
-        
-        <div class="ElasticBuilder__row2">
-            <div class="ElasticBuilder__json-template" >JSON Template</div>
-            <div class="ElasticBuilder__build" @click="$emit('onBuild')">Build</div>
-        </div>
+        <label class="ElasticBuilder__style">
+          <div class="ElasticBuilder__label-text">Style</div>
+          <div class="ElasticBuilder__label-option">{{styleFileName}}</div>
+          <input type="file" accept=".css" @change="readCss">
+        </label>
+      </div>
+      
+      <div class="ElasticBuilder__filters" ref="filters">
+        <label v-if="fields.length" v-for="(relation, idx) of config.FiltersRelation">
+          <div class="ElasticBuilder__label-text">F{{idx}}</div>
+          <select class="ElasticBuilder__label-option" @change="onSelectFilter">
+            <option v-for="(field, fieldIdx) of fields" :value="field" :selected="fieldIdx === idx">
+              {{field}}
+            </option>
+          </select>
+        </label>
+      </div>
+      
+      <div class="ElasticBuilder__row2">
+          <div class="ElasticBuilder__json-template" >JSON Template</div>
+          <div class="ElasticBuilder__build" @click="$emit('onBuild')">Build</div>
       </div>
     </div>`,
   props: {
